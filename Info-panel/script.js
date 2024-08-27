6d9514303867006e0bdac48fd0875cdb2d78a1c7
NiketSharma28 <niketsharma2805@gmail.com>
2024-08-27 20:38:46 +0530

    adding information panel

---
+++ Info-panel/script.js
@@ -0,0 +1,7 @@
+document.getElementById("hireBtn").addEventListener("click", function() {
+    window.location.href = "recruiter-question1.html";
+});
+
+document.getElementById("freelancerBtn").addEventListener("click", function() {
+    window.location.href = "freelancer-question1.html";
+});
